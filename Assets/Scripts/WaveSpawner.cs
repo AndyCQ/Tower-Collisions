a5951f1b84126baa4a7c5aa85258494dcff3b284
Nick Pham <hebenzudoaa@gmail.com>
2022-11-10 20:05:03 -0500

    crashing unity

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -8,27 +8,79 @@ public class WaveSpawner : MonoBehaviour
 {
     [SerializeField]
     private string waveFilePath;
+
+    [SerializeField]
+    private List<string> fullLevelData;
+
+    public int waveNumber = 0;
+
+    [SerializeField]
+    private int index = 0;
+    [SerializeField]
+    private bool stop = true;
+    [SerializeField]
+    private string currString = "";
+
+    public GameObject skelPrefab;
+
+    public GameObject spawn;
+    private float countdown;
+
     // Start is called before the first frame update
     void Start()
     {
-        readTextFile(waveFilePath);
+        ReadTextFile(waveFilePath);
+        PrintDebug(fullLevelData);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        countdown-=Time.deltaTime;
     }
-    void readTextFile(string file_path)
+
+    public void SpawnWave(){
+        stop = false;
+        currString=fullLevelData[index];
+        while(!stop){
+            switch(currString[0]){
+                case 'w':
+                    if(int.Parse(currString.Substring(1))!=waveNumber){
+                        stop = true;
+                        waveNumber+=1;
+                    }
+                    break;
+                case 's':
+                    int spawned=0;
+                    while(spawned<int.Parse(currString.Substring(1))){
+                        if(countdown<=0){
+                            countdown=0.5f;
+                            Instantiate(skelPrefab, spawn.transform.position, Quaternion.identity);
+                            spawned+=1;
+                        }
+                    }
+                    break;
+            }
+            index+=1;
+            currString=fullLevelData[index];
+        }
+    }
+
+
+    void ReadTextFile(string file_path)
     {
         StreamReader inp_stm = new StreamReader(file_path);
 
         while(!inp_stm.EndOfStream)
         {
             string inp_ln = inp_stm.ReadLine( );
-            print(inp_ln);
+            fullLevelData.Add(inp_ln);
         }
 
         inp_stm.Close( );  
+    }
+
+    void PrintDebug(List<string> list){
+        for(int i=0;i<list.Count;i++){
+            print(list[i]);
+        }
     }
 }
