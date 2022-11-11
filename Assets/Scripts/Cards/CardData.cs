971f013b280402dd30193f58a70722b07ce374f3
Jorge-A-Velasquez <jav494@nyu.edu>
2022-11-11 16:13:08 -0500

    Fixed up cardData

--- Assets/Scripts/Cards/CardData.cs
+++ Assets/Scripts/Cards/CardData.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 [CreateAssetMenu(fileName = "CardData", menuName = "CustomObjects/CardData", order = 0)]
-public class CardData
+public class CardData : ScriptableObject
 {
     public string cardName;
+    public Sprite cardArt;
     public GameObject towerPrefab;
 }
