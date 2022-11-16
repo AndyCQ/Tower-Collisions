90368a615804fe2c8e5e00956c11b5341190438b
Jorge-A-Velasquez <jav494@nyu.edu>
2022-11-16 18:27:51 -0500

    Added card button

---
+++ Assets/Scripts/Cards/CardButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardButton : MonoBehaviour
+{
+    public CardData currCard;
+    GameObject GM;
+    Image cardSprite;
+
+    private void Start() {
+        GM = GameObject.FindGameObjectWithTag("GameManage");
+        cardSprite = this.GetComponent<Image>();
+    }
+
+    public void SetArt() {
+        cardSprite.sprite = currCard.cardArt;
+    }
+
+    public void SetTower() {
+        GM.GetComponent<TouchPlacement>().currPrefab = currCard.towerPrefab;
+    }
+}
