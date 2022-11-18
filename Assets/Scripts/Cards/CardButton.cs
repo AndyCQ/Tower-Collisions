c12121d62afeadd59bc5e372c6f7ffda938cfd66
Jorge-A-Velasquez <jav494@nyu.edu>
2022-11-18 15:01:03 -0500

    Add button text

--- Assets/Scripts/Cards/CardButton.cs
+++ Assets/Scripts/Cards/CardButton.cs
@@ -7,15 +7,33 @@ public class CardButton : MonoBehaviour
 {
     public CardData currCard;
     GameObject GM;
+    [SerializeField]
     Image cardSprite;
+    [SerializeField]
+    TMPro.TextMeshProUGUI cardText;
+    bool setup = false;
 
     private void Start() {
         GM = GameObject.FindGameObjectWithTag("GameManager");
-        cardSprite = this.GetComponent<Image>();
+    }
+
+    private void Update() {
+        if (setup) {
+            cardText.text = currCard.cardName;
+            setup = false;
+        }
+    }
+
+    public void SetUp() {
+        setup = true;
     }
 
     public void SetArt() {
         cardSprite.sprite = currCard.cardArt;
+    }
+
+    public void SetText() {
+        cardText.text = currCard.cardName;
     }
 
     public void SetTower() {
