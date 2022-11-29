350bb3ea87818f5782e6107f38eafe1ffd068e80
NiravGolyalla <ng2411@nyu.edu>
2022-11-29 14:34:23 -0500

    Color Changing based on controller

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
 public static class Constants {
-    
+    //to get index - do elementalMatchup.index(input)
+    //To see strong against ((index+1)%3), weak ((index-1)%3)
+    public static List<string> elementalMatchup = new List<string>(){"Fire","Plant","Water"};
+    public static float normalDmgBoost = 1.25f;
+    public static float elementalDmgBoost = 2f;
+    public static float elementalResist = 0.5f;
+        
+        
 }
-    
+
---
+++ Assets/Scripts/Towers/TowerController.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    //Dropdown Choices
+    public enum DamageType {Normal,Fire,Plant,Water};
+    public enum Rariety {N,R,SR,UR};
+    public enum Tier {Bronze,Silver,Gold};
+
+
+    //Tower Customizable Variables
+    public float SetupHealth = 10f;
+    public float SetupAmmo = 10f; 
+    public float range = 10f;
+    public float fireRate = 1f;
+    public float Damage = 5f; 
+    public DamageType damageType = new DamageType();
+    //Tower in game stats
+    public Tier tier = new Tier();
+    public Rariety rariety = new Rariety();     
+
+    //Tower Settings
+    public GameObject projectile;
+    public GameObject TowerBase;
+    public GameObject firingPosition;
+    public string TargetTag = "Enemy";
+
+   //Healthbar vars
+   //public GameObject healthBarUI;
+   //public Slider slider;
+
+   //Material Change
+   public Material baseColors; 
+   public Material[] damageColor;
+   public Material[] tierColor;
+   public Material[] rarietyColor;
+   Material dM;
+   Material tM;
+   Material rM;
+    
+    void MaterialUpdate(){
+        switch(tier){
+            case Tier.Bronze:
+                tM = tierColor[0];
+                break;
+            case Tier.Silver:
+                tM = tierColor[1];
+                break;
+            case Tier.Gold:
+                tM = tierColor[2];
+                break;
+            default:
+                print("COLOR NOT FOUND");
+                break;
+        };
+        switch(rariety){
+            case Rariety.N:
+                rM = rarietyColor[0];
+                break;
+            case Rariety.R:
+                rM = rarietyColor[1];
+                break;
+            case Rariety.SR:
+                rM = rarietyColor[2];
+                break;
+            case Rariety.UR:
+                rM = rarietyColor[3];
+                break;
+            default:
+                print("COLOR NOT FOUND");
+                break;
+        };
+        switch(damageType){
+            case DamageType.Normal:
+                dM = damageColor[0];
+                break;
+            case DamageType.Fire:
+                dM = damageColor[1];
+                break;
+            case DamageType.Plant:
+                dM = damageColor[2];
+                break;
+            case DamageType.Water:
+                dM = damageColor[3];
+                break;
+            default:
+                print("COLOR NOT FOUND");
+                break;
+        };
+        Material[] newMats = {baseColors,tM,rM};
+        TowerBase.GetComponent<MeshRenderer>().materials = newMats;
+        firingPosition.GetComponent<MeshRenderer>().material = dM;
+    }
+    
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(firingPosition.transform.position,range);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaterialUpdate();
+    }
+
+    void OnValidate(){
+        MaterialUpdate();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
