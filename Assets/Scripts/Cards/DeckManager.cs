e40a1fc2e9150704a3dd6617feffc939e89e0774
Jorge-A-Velasquez <javelasquez2001@gmail.com>
2022-12-01 19:43:03 -0500

    Added main card array

--- Assets/Scripts/Cards/DeckManager.cs
+++ Assets/Scripts/Cards/DeckManager.cs
@@ -18,6 +18,7 @@ public class DeckManager : MonoBehaviour
     CardUIManager UIM;
 
     private void Start() {
+        deckList = MainCardArray.DeckArray;
         internalDeckList = new List<CardData>(deckList);
         UIM = GameObject.FindGameObjectWithTag("Canvas").GetComponent<CardUIManager>();
     }
